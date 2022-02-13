46eb85641ccf3f2c1ef013dcada84334e0ec89ce
Abner Oliveira <abnersanto2014@gmail.com>
2022-02-13 11:18:16 -0300

    Aula_11_02_22

---
+++ aula_11_02_22/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php 
+    
+        //Declarar variavel
+
+        $titulo = true; //booleano
+        $var1 = "Texto"; // String
+        $var2 = 1;// int
+        $var3 = 1.5; //double
+    
+    ?>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php 
+        //echo "TAB";
+
+        echo $titulo;
+    
+    ?></title>
+</head>
+<body>
+    <p>PHP com HTML</p>
+
+    <?php
+        //Comentarios 
+
+        // linha
+
+        # Linha
+
+        /*
+            Bloco
+            entrada de dados
+            saidade de dados
+
+
+        */
+
+        echo "Hello World!";
+        echo "<br>PHP COM HTML $var1";
+
+        print("<br>HTML/CSS/JAVASCRIPT/MYSQL/JQERY/ANGULA");
+
+    
+    ?>
+</body>
+</html>
---
+++ aula_11_02_22/index01.php
@@ -0,0 +1,33 @@
+<?php 
+
+    $n1 = 20;
+    $n2 = 30;
+
+    $resultado = $n1 + $n2;
+    
+    echo "O valor dados das variavel $n1 e $n2 são $resultado";
+
+    echo "<br>";
+
+    echo 'O valor dados das variavel $n1 e $n2 são $resultado';
+
+    echo "<br>";
+
+    echo "O valor dados das variavel ".$n1." e ".$n2." são "."$resultado";
+
+    echo "<br>";
+
+    echo 'O valor dados das variavel '.$n1.' e '.$n2.' são '.'$resultado';
+
+    echo "<br>";
+
+    echo "folha 'x' " ;
+    echo "<br>";
+    $n1 = "12";
+    $resultado = $n1 + $n2;
+    echo $n1;
+    echo "<br>";
+    echo $resultado;
+
+
+?>
